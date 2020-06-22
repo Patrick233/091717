68b7389b2061249ae2727b6e7437e62616e62d4a
Le Li <lile091717@gmail.com>
2020-06-23 00:30:12 +0200

    Add FindPhysX.cmake

---
+++ project_using_physx/cmake/FindPhysX.cmake
@@ -0,0 +1,81 @@
+
+set(TARGET_PLATFORM "linux")
+set(PhysX_COMPILER "clang")
+set(PhysX_ROOT_DIR "$ENV{HOME}/PhysX")
+set(PhysX_INSTALL_DIR "$ENV{HOME}/PhysX/physx/install")
+
+# Find include paths
+find_path(PhysX_INCLUDE_DIR PxPhysicsAPI.h
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/include/
+)
+MESSAGE("PhysX_INCLUDE_DIR: " ${PhysX_INCLUDE_DIR})
+
+find_path(PhysX_FOUNDATION_INCLUDE_DIR PsAtomic.h
+    PATHS ${PhysX_ROOT_DIR}/physx/source/foundation/include
+)
+MESSAGE("PhysX_FOUNDATION_INCLUDE_DIR: " ${PhysX_FOUNDATION_INCLUDE_DIR})
+
+find_path(PhysX_SHARED_INCLUDE_DIR foundation/PxTransform.h
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PxShared/include
+)
+MESSAGE("PhysX_SHARED_INCLUDE_DIR: " ${PhysX_SHARED_INCLUDE_DIR})
+
+# !Not a nice solution. Need to improve.
+include_directories(
+    ${PhysX_INCLUDE_DIR} 
+    ${PhysX_SHARED_INCLUDE_DIR}
+    ${PhysX_FOUNDATION_INCLUDE_DIR}
+)
+
+# Find libraries
+find_library(PhyX_CHARACTER_LIB 
+    NAMES libPhysXCharacterKinematic_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_COMMON_LIB 
+    NAMES libPhysXCommon_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_COOKING_LIB 
+    NAMES libPhysXCooking_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_EXTENTIONS_LIB 
+    NAMES libPhysXExtensions_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_FOUNDATION_LIB 
+    NAMES libPhysXFoundation_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_PVDSDK_LIB 
+    NAMES libPhysXPvdSDK_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_LIB 
+    NAMES libPhysX_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+find_library(PhyX_VEHICLE_LIB 
+    NAMES libPhysXVehicle_static_64.a
+    PATHS ${PhysX_INSTALL_DIR}/${TARGET_PLATFORM}/PhysX/bin/${TARGET_PLATFORM}.${PhysX_COMPILER}/release
+)
+
+set(PhysX_LIBS 
+    ${PhyX_LIB}
+    ${PhyX_PVDSDK_LIB}
+    ${PhyX_VEHICLE_LIB}
+    ${PhyX_CHARACTER_LIB}
+    ${PhyX_EXTENTIONS_LIB}
+    ${PhyX_COOKING_LIB}
+    ${PhyX_COMMON_LIB}
+    ${PhyX_FOUNDATION_LIB}
+)
+
